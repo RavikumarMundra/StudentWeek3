593839c23ffc42006964dcf546d6d9be6e6c14ae
Ravikumar <ravimundra29june@gmail.com>
2019-05-22 10:21:11 -0400

    added line

---
+++ src/studentweek3/StudentWeek3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author user
+ */
+public class StudentWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setFirstName("Ravikumar Mundra");
+        s1.setId(4);
+        
+        // TODO code application logic here
+    }
+    
+}
