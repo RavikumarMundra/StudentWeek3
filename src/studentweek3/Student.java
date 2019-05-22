592ba30b2834d33fdcd2cf58049ef6ffead7437b
Ravikumar <ravimundra29june@gmail.com>
2019-05-22 09:55:58 -0400

    added line

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author user
+ */
+public class Student {
+    private String firstName;
+    private int Id;
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+    
+    
+}
